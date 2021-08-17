2ab1664a72aa7ca3742379f5537301284ab10c4b
Matthew Heon <mheon@redhat.com>
2021-08-17 16:06:01 -0400

    Bump to v1.4.0-dev
    Signed-off-by: Matthew Heon <mheon@redhat.com>

--- plugins/meta/dnsname/version.go
+++ plugins/meta/dnsname/version.go
@@ -5,7 +5,7 @@ import "fmt"
 // overwritten at build time
 var gitCommit = "unknown"
 
-const dnsnameVersion = "1.3.1"
+const dnsnameVersion = "1.4.0-dev"
 
 func getVersion() string {
 	return fmt.Sprintf(`CNI dnsname plugin
